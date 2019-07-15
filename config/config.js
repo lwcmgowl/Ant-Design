cdcc70677c0f92cab59444d424edfcdf6126ad98
liwei <liwei@huimin100.cn>
2019-07-15 10:39:53 +0800

    ssss

--- config/config.js
+++ config/config.js
@@ -6,8 +6,26 @@ export default {
             // 这里暂时还没有添加配置，该插件还不会有作用，我们会在后面的课程按照需求打开相应的配置
         }],
     ],
-    routes: [{
+   routes: [{
+    path: '/',
+    component: '../layout',
+    routes: [
+      {
         path: '/',
-        component: './HelloWorld',
-    }]
+        component: 'Helloworld',
+      },
+      {
+        path: '/helloworld',
+        component: 'Helloworld'
+      },
+      {
+        path: '/dashboard',
+        routes: [
+          { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+          { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+          { path: '/dashboard/workplace', component: 'Dashboard/Workplace' }
+        ]
+      },
+    ]
+  }]
 }
