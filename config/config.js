f9562f66d7d3a01bc539e76c83451b1b26837a3b
liwei <liwei@huimin100.cn>
2019-07-17 14:51:05 +0800

    ssss

--- config/config.js
+++ config/config.js
@@ -2,7 +2,8 @@ export default {
     singular: true,
     plugins: [
         ['umi-plugin-react', {
-            antd: true
+            antd: true,
+            dva: true,
             // 这里暂时还没有添加配置，该插件还不会有作用，我们会在后面的课程按照需求打开相应的配置
         }],
     ],
@@ -26,6 +27,13 @@ export default {
           { path: '/dashboard/workplace', component: 'Dashboard/Workplace' }
         ]
       },
+      { path: 'puzzlecards', component: './puzzlecards' }
     ]
-  }]
+  }],
+  proxy: {
+        '/dev': {
+           target: 'https://08ad1pao69.execute-api.us-east-1.amazonaws.com',
+           changeOrigin: true,
+            },
+    }
 }
